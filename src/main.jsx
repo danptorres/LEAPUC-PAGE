273e08bd5bd3a9baa55630ee4aba72590c083e4e
Daniel Pires <danptorres@hotmail.com>
2025-01-28 16:23:37 -0300

    outro teste no path

--- src/main.jsx
+++ src/main.jsx
@@ -31,7 +31,7 @@ const router = createBrowserRouter([
     element: <App/>,
     children: [
       {
-        path: "/LEAPUC-page",
+        path: "/LEAPUC-page/home",
         element: <Home/>
       },
       {
